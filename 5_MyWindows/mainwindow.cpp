f5097cc2b3ed89ffac3f8e2d3a5359deb743dd40
aok <omer.kozal@metu.edu.tr>
2018-06-26 21:11:49 +0300

    5_MyWindows

---
+++ 5_MyWindows/mainwindow.cpp
@@ -0,0 +1,35 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "mydialog.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setCentralWidget(ui->plainTextEdit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_actionNew_Window_triggered()
+{
+//    MyDialog mDialog;
+
+    /* if we show it modal, we cannot turn back
+     * to main window when the dialog is open */
+//    mDialog.setModal(true);
+//    mDialog.exec();
+
+    /* if we use show, the dialog diseppears immediately after showing
+    * since function ends and returns*/
+  //  mDialog.show();
+
+    /* instead, use pointer, create dialog and set parent as "this"*/
+    mDialog = new MyDialog(this);
+    mDialog->show();
+
+}
