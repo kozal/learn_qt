2de5f369d8a1804ee72927c2ce0c7fc84d965ef3
aok <omer.kozal@metu.edu.tr>
2018-06-29 01:00:36 +0300

    update 14_QLabel

--- 14_QLabel/dialog.cpp
+++ 14_QLabel/dialog.cpp
@@ -1,11 +1,11 @@
 #include "dialog.h"
 #include "ui_dialog.h"
 
-Dialog::Dialog(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::Dialog)
+Dialog::Dialog(QWidget *parent) : QDialog(parent),
+                                  ui(new Ui::Dialog)
 {
     ui->setupUi(this);
+    ui->label->setText("<b>Hello everyone</b>");
 }
 
 Dialog::~Dialog()
